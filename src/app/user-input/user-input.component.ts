c2e31516649a3053d860c62ed5bb3a3a3278897a
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2024-06-02 12:38:23 +0530

    Adding investment model and calculate functionality.

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,25 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculator = new EventEmitter<InvestmentInput>();
 
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
-  onSubmit(){
-    console.log('Submitted')
-    console.log(this.enteredInitialInvestment)
-    console.log(this.enteredAnnualInvestment)
-    console.log(this.enteredExpectedReturn)
-    console.log(this.enteredDuration)
+  onSubmit() {
+    this.calculator.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration
+    })
   }
 }
